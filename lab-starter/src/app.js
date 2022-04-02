8b1b026e7f68a5dc38775254223021c9c2e8e5a5
micro@san <skdeep98150@gmail.com>
2022-04-01 21:52:30 -0700

    added a function to delete all div in the copy container

--- lab-starter/src/app.js
+++ lab-starter/src/app.js
@@ -1,5 +1,7 @@
 const copyContainer = document.querySelector('.copy-container')
 const orignalContainer = document.querySelectorAll('.original-container div')
+const btn = document.querySelector('button')
+
 //Clone the orginal container
 function addDiv(evt) {
     const targetDiv = evt.currentTarget.outerHTML //used outerHTML to select the div outside of the p tag
@@ -7,3 +9,12 @@ function addDiv(evt) {
     copyContainer.insertAdjacentHTML("afterbegin", targetDiv)
   }
   orignalContainer.forEach(element => element.addEventListener('click', addDiv))
+
+// Delete all copies in the copy-container
+function removeDiv(){
+    while (copyContainer.firstChild) { //added a while loop to run until there is a firstChild in copy container 
+      copyContainer.removeChild(copyContainer.firstChild); //removes the first child 
+      console.log("Div Removed") //prints after the child is removed 
+      }
+}
+btn.addEventListener('click', removeDiv)
